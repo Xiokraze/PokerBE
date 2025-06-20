b98344b443779ea2d6ecb19f6b46d9a800b036dc
MSI\jwort <xiokraze@gmail.com>
2025-06-20 17:41:40 -0400

    Enable CORS to allow front/back end to communicate.

--- PokerBE/Program.cs
+++ PokerBE/Program.cs
@@ -3,7 +3,23 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+// Add CORS policy (safe for development)
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") // React dev server
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+// Use CORS before routing
+app.UseCors();
+app.UseHttpsRedirection();
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 app.MapControllers();
